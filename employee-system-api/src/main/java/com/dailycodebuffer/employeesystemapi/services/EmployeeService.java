0073e0165d7f2534653d990283acf26456deeaf3
slh8 <sara.el-haimer@epitech.eu>
2022-10-30 11:56:44 -0400

    Get Employees

--- employee-system-api/src/main/java/com/dailycodebuffer/employeesystemapi/services/EmployeeService.java
+++ employee-system-api/src/main/java/com/dailycodebuffer/employeesystemapi/services/EmployeeService.java
@@ -1,7 +1,11 @@
 package com.dailycodebuffer.employeesystemapi.services;
 
+import java.util.List;
+
 import com.dailycodebuffer.employeesystemapi.model.Employee;
 
 public interface EmployeeService {
-    Employee createEmployee(Employee employee);
+    public Employee createEmployee(Employee employee);
+
+    public List<Employee> getAllEmployees();
 }
--- employee-system-api/src/main/java/com/dailycodebuffer/employeesystemapi/services/EmployeeServiceImpl.java
+++ employee-system-api/src/main/java/com/dailycodebuffer/employeesystemapi/services/EmployeeServiceImpl.java
@@ -1,5 +1,8 @@
 package com.dailycodebuffer.employeesystemapi.services;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
@@ -23,6 +26,21 @@ public class EmployeeServiceImpl implements EmployeeService {
         BeanUtils.copyProperties(employee, employeeEntity);
         employeeRepository.save(employeeEntity);
         return employee;
+    }
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        List<EmployeeEntity> employeeEntities = employeeRepository.findAll();
+
+        List<Employee> employees = employeeEntities
+                .stream()
+                .map(
+                        emp -> new Employee(emp.getId(),
+                                emp.getFirstName(),
+                                emp.getLastName(),
+                                emp.getEmailId()))
+                .collect(Collectors.toList());
+        return employees;
     }
 
 }
